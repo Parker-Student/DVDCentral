cb46f48e946a7bd6c7994f82d6263c804d79298f
Parker Foord <700214020@fvtc.edu>
2020-02-04 19:07:09 -0600

    Added entity framework

--- PF.DVDCentral/PF.DVDCentral.DB/Tables/tblCustomer.sql
+++ PF.DVDCentral/PF.DVDCentral.DB/Tables/tblCustomer.sql
@@ -1,4 +1,13 @@
 ﻿CREATE TABLE [dbo].[tblCustomer]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(255) NOT NULL, 
+    [City] VARCHAR(50) NOT NULL, 
+    [State] VARCHAR(50) NOT NULL, 
+    [ZIP] VARCHAR(50) NOT NULL, 
+    [Phone] VARCHAR(50) NOT NULL, 
+    [UserName] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(128) NOT NULL
 )
