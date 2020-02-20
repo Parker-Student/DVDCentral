43b5846cb832ea1a7a68f4bf67a572bc718238a9
Parker Foord <700214020@fvtc.edu>
2020-02-20 15:55:52 -0600

    fixed tables

--- PF.DVDCentral/PF.DVDCentral.DB/Tables/tblMovie.sql
+++ PF.DVDCentral/PF.DVDCentral.DB/Tables/tblMovie.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblMovie]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Title] NCHAR(10) NOT NULL, 
+    [Title] VARCHAR(128) NOT NULL, 
     [Description] VARCHAR(50) NOT NULL, 
     [ImagePath] VARCHAR(255) NOT NULL, 
     [Cost] DECIMAL(18, 2) NOT NULL, 
